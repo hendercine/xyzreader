30144fa0f5c34ffd20c43c16381dc2b8459b133b
hendercine <james@hendercine.com>
2018-05-07 19:14:41 -0700

    Modify AppBar Title transition, adapt for Lollipop and remove unneccessary code.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -3,6 +3,7 @@ package com.example.xyzreader.ui;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -24,6 +25,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -54,21 +56,19 @@ public class ArticleDetailFragment extends Fragment implements
 
     private Unbinder unbinder;
     private long mItemId;
+    private String mBody;
 
     @BindView(R.id.collapsing_toolbar_backdrop_img)
     ImageView mPhotoView;
-
-//    TODO: Clear unneccessary commented code
-//    @BindView(R.id.meta_bar)
-//    LinearLayout metaBar;
+    @BindView(R.id.toolbar_title_layout)
+    LinearLayout mToolbarTitleLayout;
     @BindView(R.id.detail_article_title)
     TextView mTitleView;
     @BindView(R.id.article_byline)
     TextView mByLineView;
     @BindView(R.id.article_body)
     TextView mBodyView;
-//    @BindView(R.id.back_btn)
-//    ImageButton mBackButton;
+    @Nullable
     @BindView(R.id.share_action)
     ImageButton mShareAction;
     @Nullable
@@ -146,6 +146,7 @@ public class ArticleDetailFragment extends Fragment implements
             return;
         }
 
+        final boolean isLollipop = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
         final String title = cursor.getString(ArticleLoader.Query.TITLE);
         final String author = Html.fromHtml(
                 DateUtils.getRelativeTimeSpanString(
@@ -154,26 +155,12 @@ public class ArticleDetailFragment extends Fragment implements
                         DateUtils.FORMAT_ABBREV_ALL).toString()
                         + " by "
                         + cursor.getString(ArticleLoader.Query.AUTHOR)).toString();
-        final String body = Html.fromHtml(cursor.getString(ArticleLoader.Query.BODY)).toString();
+        mBody = Html.fromHtml(cursor.getString(ArticleLoader.Query.BODY)).toString();
         String photo = cursor.getString(ArticleLoader.Query.PHOTO_URL);
 
         if (mCollapsingToolbarLayout != null && mToolbar != null &&
                 mAppBarLayout != null) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
-            //                int titleMaxWidth = Objects.requireNonNull(mToolbar).getWidth();
-//                int currentTitleWidth = measureTitleWidth(title);
-//
-//                if (currentTitleWidth > titleMaxWidth) {
-//                    mToolbar.setTitleTextAppearance(getContext(), R.style.AppTheme_ExpandedTitleTextAppearance_OverFlow);
-//                }
-//                    Objects.requireNonNull(mCollapsingToolbarLayout).setTitle(title);
-//                    title.length();
-//                    mCollapsingToolbarLayout.setExpandedTitleTextAppearance(
-//                            R.style.AppTheme_ExpandedTitleTextAppearance);
-//                    mCollapsingToolbarLayout.setCollapsedTitleTextAppearance
-//                            (R.style.TextAppearance_AppCompat_Title);
-//            if (mCard == null)
                 mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
                     @Override
                     public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
@@ -188,34 +175,31 @@ public class ArticleDetailFragment extends Fragment implements
                             mToolbar.setSubtitle(author);
                             mTitleView.setVisibility(View.GONE);
                             mByLineView.setVisibility(View.GONE);
+                            if (isLollipop) {
+                                activateShareButton();
+                            }
                         } else if (verticalOffset == 0) {
                             // Expanded
-                            TransitionManager.beginDelayedTransition
-                                    (mAppBarLayout, fade);
                             mCollapsingToolbarLayout.setTitleEnabled(false);
                             mToolbar.setSubtitle(null);
                             mTitleView.setText(title);
                             mByLineView.setText(author);
                             mTitleView.setVisibility(View.VISIBLE);
                             mByLineView.setVisibility(View.VISIBLE);
+                            if (isLollipop & mShareAction != null) {
+                                mShareAction.setVisibility(View.GONE);
+                            }
+                        } else {
+                            // Mid-scroll
+                            mCollapsingToolbarLayout.setTitleEnabled(true);
+                            mCollapsingToolbarLayout.setTitle(title);
+                            mToolbar.setSubtitle(author);
+                            mTitleView.setVisibility(View.GONE);
+                            mByLineView.setVisibility(View.GONE);
+                            Objects.requireNonNull(mShareAction).setVisibility(View.GONE);
                         }
                     }
                 });
-//                mToolbar.setSubtitle(author);
-//            } else {
-//                ActionBar ab = Objects.requireNonNull(getActivity()).getActionBar();
-//                if (ab != null) {
-//                    ab.setCustomView(mCollapsingToolbarLayout);
-//                    ab.setDisplayShowHomeEnabled(true);
-//                    ab.setDisplayHomeAsUpEnabled(true);
-//                    ab.setDisplayShowCustomEnabled(true);
-//                    ab.setDisplayShowTitleEnabled(true);
-//                    ab.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
-//                    ab.setTitle(R.string.detail_title_id);
-//                    ab.setSubtitle(R.string.detail_subtitle_id);
-//                }
-//                mBackButton.setVisibility(View.VISIBLE);
-//            }
 
             mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
@@ -225,7 +209,7 @@ public class ArticleDetailFragment extends Fragment implements
             });
         }
 
-        mBodyView.setText(body);
+        mBodyView.setText(mBody);
 
         Glide.with(this)
                 .load(photo)
@@ -238,14 +222,6 @@ public class ArticleDetailFragment extends Fragment implements
 
                     @Override
                     public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-//                        Bitmap bitmap = ((BitmapDrawable) resource.getCurrent()).getBitmap();
-//                        changeUIColors(bitmap);
-//                        Slide slide = new Slide();
-//                        slide.setSlideEdge(Gravity.END);
-//
-//                        TransitionManager.beginDelayedTransition(Objects.requireNonNull(mAppBarLayout), slide);
-//                        mTitleView.setVisibility(View.VISIBLE);
-
                         return false;
                     }
                 })
@@ -258,46 +234,41 @@ public class ArticleDetailFragment extends Fragment implements
                 public void onClick(View v) {
                     startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(Objects.requireNonNull(getActivity()))
                             .setType("text/plain")
-                            .setText(body)
+                            .setText(mBody)
                             .getIntent(), getString(R.string.action_share)));
                 }
             });
-        } else if (mShareAction != null){
-            mShareAction.setVisibility(View.VISIBLE);
-            mShareAction.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(Objects.requireNonNull(getActivity()))
-                            .setType("text/plain")
-                            .setText(body)
-                            .getIntent(), getString(R.string.action_share)));
-                }
-            });
+        } else if (mShareAction != null && !isLollipop){
+            // Display share action button
+            activateShareButton();
 
         }
     }
 
+    // Required method
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) { }
 
+    // Unbind views for ButterKnife
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
     }
 
-//    private void changeUIColors(Bitmap bitmap) {
-//        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-//            @Override
-//            public void onGenerated(@NonNull Palette palette) {
-//                int defaultColor = 0x673AB7;
-//                int darkMutedColor = palette.getDarkMutedColor(defaultColor);
-//                metaBar.setBackgroundColor(darkMutedColor);
-//                if (mCollapsingToolbarLayout != null) {
-//                    mCollapsingToolbarLayout.setContentScrimColor(darkMutedColor);
-//                    mCollapsingToolbarLayout.setStatusBarScrimColor(darkMutedColor);
-//                }
-//            }
-//        });
-//    }
+    // Helper method to display a functional share button in the toolbar.
+    private void activateShareButton() {
+        if (mShareAction != null) {
+            mShareAction.setVisibility(View.VISIBLE);
+        }
+        mShareAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(Objects.requireNonNull(getActivity()))
+                        .setType("text/plain")
+                        .setText(mBody)
+                        .getIntent(), getString(R.string.action_share)));
+            }
+        });
+    }
 }
