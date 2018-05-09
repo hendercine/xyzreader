d0b2ab1302484c97283b9bb4d203fd642872e263
hendercine <james@hendercine.com>
2018-05-08 20:34:58 -0700

    Resolve last TODOs.

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -1,6 +1,8 @@
 package com.example.xyzreader.remote;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,13 +10,15 @@ import java.net.URL;
 public class Config {
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
+    private static Context context;
 
     static {
         URL url = null;
         try {
             url = new URL("https://go.udacity.com/xyz-reader-json" );
         } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
+            Toast.makeText(context, "Please check your internet connection.",
+                    Toast.LENGTH_LONG).show();
             Log.e(TAG, "Please check your internet connection.");
         }
 
